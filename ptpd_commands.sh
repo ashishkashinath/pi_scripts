f48ef00482bc6c193a3f65557c1a100ea7cbd3aa
Ashish Kashinath <akashina@eng.ucsd.edu>
2021-11-03 15:14:48 -0500

    Update ptpd_commands.sh

--- ptpd_commands.sh
+++ ptpd_commands.sh
@@ -1,6 +1,7 @@
 # This is to be added at the end of the bashrc file
 
 # At the master
-sudo taskset 0x2 sudo ptpd2 --interface eth0 --masteronly --unicast -u <Address of slave> --debug --foreground --verbose
+sudo taskset 0x2 sudo ptpd2 --interface eth0 --masteronly --unicast -u <Address of Slave> --debug --foreground --verbose
 
 # At the slave
+sudo taskset 0x2 sudo ptpd2 --interface eth0 --slaveonly --unicast -u <Address of Master> --debug --foreground --verbose
